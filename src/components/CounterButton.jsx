1030dc2dda660fa878917b592763b43ea6ea081c
Boyan Zhelev <boyan.zhelev@mkit.io>
2022-09-30 00:15:14 +0300

    fix: typo

--- src/components/CounterButton.jsx
+++ src/components/CounterButton.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
-export default function CounterButton({hasCustomNumber: usesCustomNumber, handler, name}){
+export default function CounterButton({hasCustomNumber, handler, name}){
     const [customNum, setCustomNum] = useState(1);
 
     const onChange = (event) => setCustomNum(event.target.value);
-    const onClick = usesCustomNumber ? handler(customNum) : handler;
+    const onClick = hasCustomNumber ? handler(customNum) : handler;
 
     return (
         <div className='counter-control-container'>
-            { usesCustomNumber && <input type="number" placeholder='Provide a number' onChange={onChange} /> }
+            { hasCustomNumber && <input type="number" placeholder='Provide a number' onChange={onChange} /> }
             <button onClick={onClick}>{name}</button>
         </div>
     )
